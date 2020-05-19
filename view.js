95652f33002b5e75c8cd258cc545ec3cadf20345
Pavel Lapin <tnrs86@yandex.ru>
2020-05-19 09:43:56 +0300

    View module: add create method of the interface

--- view.js
+++ view.js
@@ -1,49 +1,63 @@
-"use strict";
+'use strict';
 
 class View {    
     
     constructor(options, rootElement) {
+        //данные с координатами элементов управления
+
+        //данные для отображения
+
+
         //задать классы для элементов управления
-        this.sliderElements = new Map(); //хранение объектов JQ по схеме {родитель: потомок}
+
         this.rootElement = rootElement;
         
         this.slider = new Slider('slider', '', options.verticalView);
         let sliderJQObject = this.slider.jqObject;
-        this.sliderElements.set(sliderJQObject, rootElement);
-        
+        this.slider.addToPage(this.rootElement);        
+
         this.track = new Track('track', '', options.verticalView);
         let trackJQObject = this.track.jqObject;
-        this.sliderElements.set(trackJQObject, sliderJQObject);
-        //установка слушателя
+        this.track.addToPage(this.slider.jqObject);
 
         this.leftThumb = new Thumb('leftThumb', 'left-thumb');
         let leftThumbJQObject = this.leftThumb.jqObject;
-        this.sliderElements.set(leftThumbJQObject, trackJQObject);
+        this.leftThumb.addToPage(this.track.jqObject);
 
         this.rightThumb = new Thumb('rightThumb', 'right-thumb');
         let rightThumbJQObject = this.rightThumb.jqObject;
-        this.sliderElements.set(rightThumbJQObject, trackJQObject);
+        this.rightThumb.addToPage(this.track.jqObject);
 
         this.thumbIsPressed = false;
-        
-        this.clickCounter = 0;
-        //построение нового экземпляра слайдера
-    }    
 
-    createSlider() {
-        this.sliderElements.forEach(function(parent, child) {           
-            $(parent).append(child);
-        })
+
+        function createInterface() {
+
+        }
+
+        
+        //построение нового экземпляра слайдера
+    }
+
+    addListeners(parent, func) {
+        //метод, устанавливающий слушателей на необходимые объекты
+        this.track.setListener(parent, func);        
+        this.leftThumb.setListener(parent);
+        this.rightThumb.setListener(parent);
+    }
+    
+    changeInterface() {
         
     }
 
-    addListeners(parent) {
-        //метод, устанавливающий слушателей на необходимые объекты
-        this.track.setListener(parent);        
-        this.leftThumb.setListener(parent);
-        this.rightThumb.setListener(parent);
-    }    
+    //функция для получения текущей позиции
+    getCurrentPosition() {
+        console.log(this.slider.jqObject.offset().left);
+    }
 
+    setCurrentPosition() {
+
+    }
 }
 
 class Slider {
@@ -66,7 +80,11 @@ class Slider {
 
     setListener () {
 
-    }    
+    }
+
+    addToPage(parent) {
+        $(parent).append(this.jqObject);
+    }
 
 }
 
@@ -88,20 +106,30 @@ class Track {
         this.jqObject = $(this.html);
     }
 
-    setListener (parent) {
+    addToPage(parent) {
+        $(parent).append(this.jqObject);
+    }
+
+    setListener (parent, func) {
         
-        this.jqObject.mousemove(function(){
+        this.jqObject.mousemove(function(event){
             
             if (parent.thumbIsPressed) {
                 //вызов вункции в presenter
-                console.log('курсор перемещен и нажат')
+                func(event.pageX);
             }
             //передача данных в Presenter перемещение курсора
         })
 
         this.jqObject.mouseout(function(){
-            console.log('фокус потерян')
-            //передача данных в Presenter перемещение курсора)
+            
+            
+        })
+
+        this.jqObject.click(function(){
+            
+            //передача данных в Presenter
+            // перемещение курсора)
         })
     }   
 
@@ -123,20 +151,27 @@ class Thumb {
         this.jqObject = $(this.html);
     }
 
+    addToPage(parent) {
+        $(parent).append(this.jqObject);
+    }
+
     setListener(parent) {
         
         this.jqObject.mousedown(function(){
             
             parent.thumbIsPressed = true;
-            //передача данных в Presenter (нажата пуля)
-                      
+            //передача данных в Presenter (нажата пуля)                      
         })
 
         this.jqObject.mouseup(function(){
             parent.thumbIsPressed = false;
             //передача данных в Presenter (нажата пуля)
-
         })
+    }
+
+    moveToPoint(point) {
+        //перемещение пули к нужной точке
+
     }
 
 }
@@ -152,6 +187,10 @@ class Filler {
 
         this.name = name;
         this.html = `<div class="${className}"></div>`;
+    }
+
+    addToPage(parent) {
+        $(parent).append(this.jqObject);
     }
 
     get JQObject() {
