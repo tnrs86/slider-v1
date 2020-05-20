4bb3d2ff9ea3f395564cf7186eae36f89984c3c1
Pavel Lapin <tnrs86@yandex.ru>
2020-05-20 08:17:08 +0300

    View module: adjusting the structure

--- view.js
+++ view.js
@@ -9,50 +9,94 @@ class View {
 
 
         //задать классы для элементов управления
-
         this.rootElement = rootElement;
-        
-        this.slider = new Slider('slider', '', options.verticalView);
-        let sliderJQObject = this.slider.jqObject;
-        this.slider.addToPage(this.rootElement);        
-
-        this.track = new Track('track', '', options.verticalView);
-        let trackJQObject = this.track.jqObject;
-        this.track.addToPage(this.slider.jqObject);
-
-        this.leftThumb = new Thumb('leftThumb', 'left-thumb');
-        let leftThumbJQObject = this.leftThumb.jqObject;
-        this.leftThumb.addToPage(this.track.jqObject);
-
-        this.rightThumb = new Thumb('rightThumb', 'right-thumb');
-        let rightThumbJQObject = this.rightThumb.jqObject;
-        this.rightThumb.addToPage(this.track.jqObject);
-
         this.thumbIsPressed = false;
+        this.verticalView = options.verticalView;
+        createInterface(this);
 
-
-        function createInterface() {
-
-        }
-
-        
         //построение нового экземпляра слайдера
+        function createInterface(self) {
+            self.slider = new Slider('slider', '', self.verticalView);
+            //let sliderJQObject = this.slider.jqObject;
+            self.slider.addToPage(self.rootElement);        
+    
+            self.track = new Track('track', '', self.verticalView);
+            //let trackJQObject = this.track.jqObject;
+            self.track.addToPage(self.slider.jqObject);
+    
+            self.leftThumb = new Thumb('leftThumb', 'left-thumb');
+            //let leftThumbJQObject = this.leftThumb.jqObject;
+            self.leftThumb.addToPage(self.track.jqObject);
+    
+            self.rightThumb = new Thumb('rightThumb', 'right-thumb');
+            //let rightThumbJQObject = this.rightThumb.jqObject;
+            self.rightThumb.addToPage(self.track.jqObject);
+            
+            //добавить поля вывода текущих данных
+
+            //добавить шкалу (scale)
+        }
+        
     }
 
-    addListeners(parent, func) {
+    //Блок создания интерфейса
+    addListeners(view, func) {
         //метод, устанавливающий слушателей на необходимые объекты
-        this.track.setListener(parent, func);        
+        /*this.track.setListener(parent, func);        
         this.leftThumb.setListener(parent);
-        this.rightThumb.setListener(parent);
+        this.rightThumb.setListener(parent);*/
+        view.track.setListener(view, func);        
+        view.leftThumb.setListener(view);
+        view.rightThumb.setListener(view);
     }
     
+    createScaleElement() {
+        //перед эти
+
+        //отрисовка элементов шкалы
+
+        //нанесение данных на шкалу
+
+    }    
+
+    //блок работы интерфейса
     changeInterface() {
         
+        //перемещение пуль в соответствии с начальными/текущими значениями выбранного диапазона
+
+        //внесение измененных данных в элементы управления
     }
 
     //функция для получения текущей позиции
     getCurrentPosition() {
-        console.log(this.slider.jqObject.offset().left);
+        //передача массива с текущими координатами основных элементов в зависимости от ориентации слайдера:
+            //начало и ширина трека слайдера
+            //координата левой пули
+            //координата правой пули
+        
+        let currentPosition = [2];
+        let leftSlider = 0;
+        let widthSlider = 0;
+        let leftThumbLeft = 0;
+        let rightThumbLeft = 0;
+        if (!this.verticalView) {
+            leftSlider = this.slider.jqObject.offset().left;
+            widthSlider = this.slider.jqObject.width();
+            leftThumbLeft = this.leftThumb.jqObject.offset().left;
+            rightThumbLeft = this.rightThumb.jqObject.offset().left;
+        } else {
+            leftSlider = this.slider.jqObject.offset().top;
+            widthSlider = this.slider.jqObject.height();
+            leftThumbLeft = this.leftThumb.jqObject.offset().top;
+            rightThumbLeft = this.rightThumb.jqObject.offset().top;
+        }
+        
+        currentPosition[0] = (leftThumbLeft - leftSlider) / widthSlider;
+        currentPosition[1] = (rightThumbLeft - leftSlider) / widthSlider;
+        
+        
+
+        return currentPosition;
     }
 
     setCurrentPosition() {
