9af58b3af8bd581e1b63665750e8b27f1f0b16e4
Pavel Lapin <tnrs86@yandex.ru>
2020-05-21 22:56:44 +0300

    Model module: adding links to presenter

--- model.js
+++ model.js
@@ -7,15 +7,13 @@ class Model {
         this.startRange;
         this.endRange;
         this.sliderScale = calcSliderScale();
-        
-        //установка внешних слушателей
-        if (options.inputStart) {
-            setExternalListener(options.inputStart);
-        }
+        this.inputStart = options.inputStart;
+        this.inputEnd = options.inputEnd;
 
-        if (options.inputEnd) {
-            setExternalListener(options.inputEnd);
-        }
+        let needFunction = this.modelToPresenterTransfer.bind(this);
+
+        //установка внешних слушателей
+
 
         //функция расчета шкалы слайдера (числовые значения)
         function calcSliderScale() {
@@ -24,28 +22,54 @@ class Model {
         };
 
         //функция установки внешних слушателей
-        function setExternalListener(input) {
-            input.change(function() {
-                console.log(this.value)
-            })
-        }
+
     }
 
-    getCurrentValue() {
+    setExternalListener(model, presenterFunction) {
+        
+        if (model.inputStart) {
+            model.inputStart.change(function() {                
+                presenterFunction(model.inputStart.val(), 'inputStart')
+        })
+        }
+
+        if (model.inputEnd) {
+            model.inputEnd.change(function() {
+                presenterFunction(model.inputEnd.val(), 'inputEnd')
+            })
+        }
+
+    }
+
+    /*getCurrentValue() {
         //функция передает текущие значения установленного диапазона во внешний модуль (input и т.п.)
         let range = {};
         range.start = this.startRange;
         range.end = this.endRange;
 
         return range;
-    }
+    }*/
 
     setCurrentValue() {
         //функция получает данные об изменениях во внешнем модуле (input и т.п.)
+
     }
 
-    getDataFromModel() {
+    getDataFromModel(value, modulesBounds) {
         //функция взаимодействия model и presenter
+        let key = Object.keys(value)[0];
+
+        //this.inputStart.val(modulesBounds[keys[0]]);
+        this[modulesBounds[key]].val(value[key]);
+                //передача данных во внешнюю среду
     }
+
+    //обработка данных из Presenter
+
+    modelToPresenterTransfer(extData) {
+
+    }
+
+    //передача данных в Presenter
 
 }
